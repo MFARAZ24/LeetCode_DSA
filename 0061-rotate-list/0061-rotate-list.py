42478a4082bffb0b25a53230c9c1c4c0c4716939
Muhammad Faraz <119207160+MFARAZ24@users.noreply.github.com>
2025-12-23 21:55:43 -0600

    Time: 3 ms (15.32%), Space: 17.4 MB (95.95%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -5,71 +5,37 @@
 #         self.next = next
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        # if not head or not head.next or k == 0:
-        #     return head
-        # # length of linkedlist
-        # length = 0
-        # node = head
-        # while node:
-        #     length+=1
-        #     node = node.next
-        # # check valid k
-        # if k>length:
-        #     k = k%length
-        # if k == 0:
-        #     return head
-        # fast = ListNode()
-        # fast.next = head
-        # slow = ListNode()
-        # slow.next = head
-            
-        # # Move fast pointer by K places
-
-        # while k>0:
-        #     fast = fast.next
-        #     k = k-1
-
-        # # Move fast and slow pointer until you reach last node
-
-        # while fast and fast.next:
-        #     fast = fast.next
-        #     slow = slow.next
-        # # Manipulate pointer and return the list
-
-        # fast.next = head
-        # head = slow.next
-        # slow.next = None
-        # return head
         if not head or not head.next or k == 0:
             return head
-
-        # Compute length
+        # length of linkedlist
         length = 0
         node = head
         while node:
-            length += 1
+            length=length+1
             node = node.next
-
-        k = k % length
+        # check valid k
+        k = k%length
         if k == 0:
             return head
-
+        
         fast = head
-        slow = head
+        slow= head
+        
+        # Move fast pointer by K places
 
-        # Move fast k steps
-        while k > 0:
+        while k>0:
             fast = fast.next
-            k -= 1
+            k = k-1
 
-        # Move both until fast is at last node
+        # Move fast and slow pointer until you reach last node
+
         while fast.next:
             fast = fast.next
             slow = slow.next
-
-        # Rotate
+        # Manipulate pointer and return the list
+        
         fast.next = head
         head = slow.next
         slow.next = None
-
         return head
+        
